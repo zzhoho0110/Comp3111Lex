b5de09aac6c53613dfda3b27427489789701eb89
user <hoho0110@live.hk>
2024-02-17 17:49:40 +0800

    Added the 3rd print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -6,6 +6,7 @@ public class mainApp1 {
         int n = 11;
         System.out.println("Welcome to Scientific Calculator");
         System.out.println("Program started");
+        System.out.println("---------------------");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = "+ myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
